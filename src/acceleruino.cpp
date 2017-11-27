5a9cd3999c4b2f252c714b6f33d8b2c43acf6c4d
Kevin Skidmore <k.skidmore@bis-school.com>
2017-11-27 19:33:36 +0100

    Added full proof of concept functionality, still needs debugging

--- src/acceleruino.cpp
+++ src/acceleruino.cpp
@@ -4,6 +4,7 @@
 
 #include <SdFat.h>
 #include "RTClib.h"
+#include <Adafruit_FONA.h>
 
 #define RANGE LIS3DH_RANGE_2_G  // LIS3DH_RANGE_<N>_G where N in (2, 4, 8, 16)
 /* Valid Rate Values
@@ -21,7 +22,7 @@ LIS3DH_DATARATE_LOWPOWER_5KHZ  =  0b1001
 #define RATE LIS3DH_DATARATE_25_HZ
 
 const int chipSelect = 4;
-volatile int stopFlag = 0;
+volatile int waitFlag = 0;
 
 String dataString = "";
 File dataFile;
@@ -30,12 +31,22 @@ SdFat SD;
 
 int accelIntPin = 16;
 int LIS3DH_ADDR = 0x18; // change this to 0x19 for alternative i2c address
-int reading = 0; //counter for number of readings taken - for diagnostic use
+int reading = 0; //counter for number of readings to take
+int READ_LIMIT = 100;
 
 // I2C
 Adafruit_LIS3DH accel = Adafruit_LIS3DH();
 
 RTC_DS3231 rtc;
+
+#define FONA_RX 2
+#define FONA_TX 3
+#define FONA_RST 4
+
+#define PIN 9170
+
+HardwareSerial *fonaSerial = &Serial1;
+Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
 
 void sd_dateTime_callback(uint16_t* date, uint16_t* time) {
 
@@ -65,9 +76,22 @@ void writeAccelRegister(byte reg, byte data) {
   Wire.endTransmission();
 }
 
-DateTime getGPRSDateTime() {
+DateTime getGPRSDateTime() {  // TBD
 
   return DateTime(F(__DATE__), F(__TIME__));
+
+}
+char[] getGPRSLocation(void) {
+  uint16_t returncode;
+  char location[255];
+  if (!fona.getGSMLoc(&returncode, location, 250))
+    Serial.println(F("Failed!"));
+  if (returncode == 0) {
+    Serial.println(replybuffer);
+  } else {
+    Serial.print(F("Fail code #")); Serial.println(returncode);
+  }
+  return location;
 
 }
 void logAccelData(int x, int y, int z) {
@@ -76,12 +100,21 @@ void logAccelData(int x, int y, int z) {
   char ts[18];
   snprintf(ts, sizeof(ts), "%i-%i-%i %i%i%i", logtime.year(), logtime.month(), logtime.day(), logtime.hour(), logtime.minute(), logtime.second());
 
+  if(!FONA_ON) {
+    fona_power_on();
+  }
+
+  char loc = getGPRSLocation();
+
   Serial.print(ts);
+  Serial.print("\tLoc: "); Serial.print(loc);
   Serial.print("  \tX:  "); Serial.print(x);
   Serial.print("  \tY:  "); Serial.print(y);
   Serial.print("  \tZ:  "); Serial.println(z);
 
   if (dataFile) {
+    dataFile.print(loc);
+    dataFile.print(",");
     dataFile.print(ts);
     dataFile.print(",");
     dataFile.print(x);
@@ -95,7 +128,7 @@ void logAccelData(int x, int y, int z) {
   }
   else {
     Serial.println("File not open");
-    stopFlag = 1;
+    waitFlag = 1;
     return;
   }
 
@@ -104,21 +137,139 @@ void logAccelData(int x, int y, int z) {
 
 void init_ACC(void)
 {
-    // configurations for control registers
-    writeAccelRegister(0x20, 0x57); //Write A7h into CTRL_REG1;      // Turn on the sensor, enable X, Y, Z axes with ODR = 100Hz normal mode.
-    writeAccelRegister(0x21, 0x09); //Write 09h into CTRL_REG2;      // High-pass filter (HPF) enabled
-    writeAccelRegister(0x22, 0x40); //Write 40h into CTRL_REG3;      // ACC AOI1 interrupt signal is routed to INT1 pin.
-    writeAccelRegister(0x23, 0x00); //Write 00h into CTRL_REG4;      // Full Scale = +/-2 g
-    writeAccelRegister(0x24, 0x08); //Write 08h into CTRL_REG5;      // Default value is 00 for no latching. Interrupt signals on INT1 pin is not latched.
-                                                                //Users don’t need to read the INT1_SRC register to clear the interrupt signal.
-    // configurations for wakeup and motionless detection
-    writeAccelRegister(0x32, 0x10); //Write 10h into INT1_THS;          // Threshold (THS) = 16LSBs * 15.625mg/LSB = 250mg.
-    writeAccelRegister(0x33, 0x00); //Write 00h into INT1_DURATION;     // Duration = 1LSBs * (1/10Hz) = 0.1s.
-    //readRegister();  //Dummy read to force the HP filter to set reference acceleration/tilt value
-    writeAccelRegister(0x30, 0x2A); //Write 2Ah into INT1_CFG;          // Enable XLIE, YLIE, ZLIE interrupt generation, OR logic.
+  // BEGIN Accelerometer Initialization
+  Serial.println("Initializing accelerometer....");
+  int RETRY_VAL = 10;
+  int cnt = 0;
+  while (!accel.begin(LIS3DH_ADDR)) {
+    Serial.print("Error inializing accelerometer...");
+    Serial.println("retrying in 1 second");
+    delay (1000);
+    cnt++;
+    if(cnt > RETRY_VAL) {
+      waitFlag = 1;
+      Serial.println("Accelerometer failed to initialize");
+      return;
+    }
+  }
+
+  // configurations for control registers
+  writeAccelRegister(0x20, 0x57); //Write A7h into CTRL_REG1;      // Turn on the sensor, enable X, Y, Z axes with ODR = 100Hz normal mode.
+  writeAccelRegister(0x21, 0x09); //Write 09h into CTRL_REG2;      // High-pass filter (HPF) enabled
+  writeAccelRegister(0x22, 0x40); //Write 40h into CTRL_REG3;      // ACC AOI1 interrupt signal is routed to INT1 pin.
+  writeAccelRegister(0x23, 0x00); //Write 00h into CTRL_REG4;      // Full Scale = +/-2 g
+  writeAccelRegister(0x24, 0x08); //Write 08h into CTRL_REG5;      // Default value is 00 for no latching. Interrupt signals on INT1 pin is not latched.
+                                                              //Users don’t need to read the INT1_SRC register to clear the interrupt signal.
+  // configurations for wakeup and motionless detection
+  writeAccelRegister(0x32, 0x10); //Write 10h into INT1_THS;          // Threshold (THS) = 16LSBs * 15.625mg/LSB = 250mg.
+  writeAccelRegister(0x33, 0x00); //Write 00h into INT1_DURATION;     // Duration = 1LSBs * (1/10Hz) = 0.1s.
+  //readRegister();  //Dummy read to force the HP filter to set reference acceleration/tilt value
+  writeAccelRegister(0x30, 0x2A); //Write 2Ah into INT1_CFG;          // Enable XLIE, YLIE, ZLIE interrupt generation, OR logic.
+
+  Serial.print("Range = "); Serial.print(2 << accel.getRange());
+  //Serial.print(accel.getRange(), BIN);
+  Serial.println("G");
+
+  Serial.println("Success initializing accelerometer");
+  // END Accelerometer initialization
+}
+
+void init_RTC(void) {
+
+  // BEGIN RTC initialization
+  Serial.println("Initializing DS3231 RTC");
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    waitFlag = 1;
+    return;
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, lets set the time!");
+    rtc.adjust(getGPRSDateTime());
+  }
+  Serial.println("Success initializing RTC");
+  // END RTC initialization
+
+}
+void init_SD(void) {
+
+  // BEGIN SD Card initialization
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect, SD_SCK_MHZ(50))) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    waitFlag = 1;
+    return;
+  }
+  Serial.println("card initialized.");
+  // END SD Card initialization
 
 }
 
+void init_FONA(void) {
+  fonaSerial->begin(4800);  // does this need to be in the fona_power_on function?
+}
+
+void fona_power_off(void) {
+
+  digitalWrite(FONA_KEY, LOW);
+  delay(2000);
+  digitalWrite(FONA_KEY, HIGH);
+  //delay(3000);
+}
+void fona_power_on(void) {
+
+  digitalWrite(FONA_KEY, LOW);
+  delay(2000);
+  digitalWrite(FONA_KEY, HIGH);
+  delay(3000);
+  if (fona.begin(*fonaSerial)) {
+    fona.setGPRSNetworkSettings(F("web.vodafone.de"));
+    if (!fona.enableGPRS(true)) {
+      Serial.println(F("Failed to turn GPRS on"));
+    }
+    uint8_t n = fona.getNetworkStatus();
+    Serial.print(F("Network status "));
+    Serial.print(n);
+    Serial.print(F(": "));
+    if (n == 0) Serial.println(F("Not registered"));
+    if (n == 1) Serial.println(F("Registered (home)"));
+    if (n == 2) Serial.println(F("Not registered (searching)"));
+    if (n == 3) Serial.println(F("Denied"));
+    if (n == 4) Serial.println(F("Unknown"));
+    if (n == 5) Serial.println(F("Registered roaming"));
+
+  }
+  else {
+    Serial.println(F("Couldn't find FONA"));
+  }
+
+}
+char[] getGPRSLocation(void) {
+  uint16_t returncode;
+  char location[255];
+  if (!fona.getGSMLoc(&returncode, location, 250))
+    Serial.println(F("Failed!"));
+  if (returncode == 0) {
+    Serial.println(replybuffer);
+  } else {
+    Serial.print(F("Fail code #")); Serial.println(returncode);
+  }
+  return location;
+
+}
+uint8_t is_accel_interrupt(void) {
+  return digitalRead(accelIntPin) == HIGH;
+}
+uint8_t is_rtc_interrupt(void) {  // not yet implemented
+  return 0;
+}
+void logFifoData(void) {  // not yet implemented
+  Serial.println("logging FIFO data");
+}
 
 void setup(void) {
 
@@ -132,59 +283,10 @@ void setup(void) {
   Serial.println("##############################");
   Serial.println();
 
-  // BEGIN Accelerometer Initialization
-  Serial.println("Initializing accelerometer....");
-  int RETRY_VAL = 10;
-  int cnt = 0;
-  while (!accel.begin(LIS3DH_ADDR)) {
-    Serial.print("Error inializing accelerometer...");
-    Serial.println("retrying in 1 second");
-    delay (1000);
-    cnt++;
-    if(cnt > RETRY_VAL) {
-      stopFlag = 1;
-      Serial.println("Accelerometer failed to initialize");
-      return;
-    }
-  }
   init_ACC();
-
-  Serial.print("Range = "); Serial.print(2 << accel.getRange());
-  //Serial.print(accel.getRange(), BIN);
-  Serial.println("G");
-
-  Serial.println("Success initializing accelerometer");
-  // END Accelerometer initialization
-
-  delay(6000);
-
-  // BEGIN RTC initialization
-  Serial.println("Initializing DS3231 RTC");
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    stopFlag = 1;
-    return;
-  }
-
-  if (rtc.lostPower()) {
-    Serial.println("RTC lost power, lets set the time!");
-    rtc.adjust(getGPRSDateTime());
-  }
-  Serial.println("Success initializing RTC");
-  // END RTC initialization
-
-  // BEGIN SD Card initialization
-  Serial.print("Initializing SD card...");
-
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect, SD_SCK_MHZ(50))) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    stopFlag = 1;
-    return;
-  }
-  Serial.println("card initialized.");
-  // END SD Card initialization
+  init_RTC();
+  init_SD();
+  init_FONA();
 
   // BEGIN creating new logfile
   DateTime now = rtc.now();
@@ -202,7 +304,7 @@ void setup(void) {
   }
   else {
     Serial.println("Error opening file");
-    stopFlag = 1;
+    waitFlag = 1;
     return;
   }
   // END logfile creation
@@ -211,21 +313,32 @@ void setup(void) {
 
 void loop() {
 
-  //stopFlag = 1;
-  while(stopFlag);
+  //waitFlag = 1;
+  while(waitFlag);
 
-  accel.read();      // get X Y and Z data at once
-
-  if(digitalRead(accelIntPin) == HIGH) {
-    Serial.print("  \tinterrupt: ");
+  if(is_accel_interrupt()) {
+    Serial.print("accel interrupt: ");
     Serial.print(reading++); Serial.print(",  ");
     Serial.print(readAccelRegister(0x21)); Serial.print(",  "); //read register to reset high-pass filter
     Serial.print(readAccelRegister(0x26)); Serial.print(",  "); //read register to set reference acceleration
     Serial.print(readAccelRegister(LIS3DH_REG_INT1SRC)); Serial.print(",  "); //Read INT1_SRC to de-latch;
+    Serial.println();
+    if(reading > READ_LIMIT) { // only log FIFO data if you are not already logging
+      logFifoData();
+    }
+    reading = 0;
   }
-  Serial.println();
 
-  logAccelData(accel.x, accel.y, accel.z);
+  if(is_rtc_interrupt()) {
+    Serial.println("timer based interrupt");
+    reading = 0;
+  }
+
+  if(reading < READ_LIMIT) {
+    accel.read();
+    logAccelData(accel.x, accel.y, accel.z);
+    reading++;
+  }
 
 
   delay(1);
